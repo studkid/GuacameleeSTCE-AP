2171e33a93d99ee1e83c064c74b6e29cb6ca2a7e
Elly <eric.poore@gmail.com>
2025-03-09 23:26:11 -0500

    Initial Push

--- MyGameAP/App.xaml.cs
+++ MyGameAP/App.xaml.cs
@@ -9,49 +9,44 @@ using Archipelago.MultiClient.Net.MessageLog.Messages;
 using Newtonsoft.Json;
 using Serilog;
 
-namespace MyGameAP
-{
-    public partial class App : Application
-    {
+namespace MyGameAP {
+    public partial class App : Application {
         static MainPageViewModel Context;
         public static ArchipelagoClient Client { get; set; }
         private DeathLinkService _deathlinkService;
         private static readonly object _lockObject = new object();
-        public App()
-        {
+
+        public App() {
             InitializeComponent();
 
             Context = new MainPageViewModel();
             Context.ConnectClicked += Context_ConnectClicked;
-            Context.CommandReceived += (e, a) =>
-            {
+            Context.CommandReceived += (e, a) => {
                 Client?.SendMessage(a.Command);
             };
             MainPage = new MainPage(Context);
             Context.ConnectButtonEnabled = true;
         }
-        private async void Context_ConnectClicked(object? sender, ConnectClickedEventArgs e)
-        {
+
+        private async void Context_ConnectClicked(object? sender, ConnectClickedEventArgs e) {
             Context.ConnectButtonEnabled = false;
             Log.Logger.Information("Connecting...");
-            if (Client != null)
-            {
+            if (Client != null)  {
                 Client.Connected -= OnConnected;
                 Client.Disconnected -= OnDisconnected;
                 Client.ItemReceived -= Client_ItemReceived;
                 Client.MessageReceived -= Client_MessageReceived;
-                if (_deathlinkService != null)
-                {
+                if (_deathlinkService != null) {
                     _deathlinkService.OnDeathLinkReceived -= _deathlinkService_OnDeathLinkReceived;
                     _deathlinkService = null;
                 }
                 Client.CancelMonitors();
             }
-            GenericGameClient client = new GenericGameClient("MyGame");
+
+            GenericGameClient client = new GenericGameClient("Game");
             var connected = client.Connect();
-            if (!connected)
-            {
-                Log.Logger.Error("Dark Souls not running, open Dark Souls before connecting!");
+            if (!connected) {
+                Log.Logger.Error("Guacamelee not running, open Guacamelee before connecting!");
                 Context.ConnectButtonEnabled = true;
                 return;
             }
@@ -61,50 +56,48 @@ namespace MyGameAP
             Client.Connected += OnConnected;
             Client.Disconnected += OnDisconnected;
 
-            await Client.Connect(e.Host, "My Game");
+            await Client.Connect(e.Host, "Clique");
 
             Client.ItemReceived += Client_ItemReceived;
             Client.MessageReceived += Client_MessageReceived;
 
             await Client.Login(e.Slot, !string.IsNullOrWhiteSpace(e.Password) ? e.Password : null);
 
-            if (Client.Options.ContainsKey("EnableDeathlink") && (bool)Client.Options["EnableDeathlink"])
-            {
-                var _deathlinkService = Client.EnableDeathLink();
-                _deathlinkService.OnDeathLinkReceived += _deathlinkService_OnDeathLinkReceived;
-                //ToDo listen for player death
-            }
+            //if (Client.Options.ContainsKey("EnableDeathlink") && (bool)Client.Options["EnableDeathlink"]) {
+            //    var _deathlinkService = Client.EnableDeathLink();
+            //    _deathlinkService.OnDeathLinkReceived += _deathlinkService_OnDeathLinkReceived;
+            //    //ToDo listen for player death
+            //}
 
             var myLocations = Helpers.GetLocations();
 
-            var goalLocation = myLocations.First(x => x.Name.Contains("GoalLocationName"));
-            Memory.MonitorAddressBitForAction(goalLocation.Address, goalLocation.AddressBit, () => Client.SendGoalCompletion());
+            //var goalLocation = myLocations.First(x => x.Name.Contains("GoalLocationName"));
+            //Memory.MonitorAddressBitForAction(goalLocation.Address, goalLocation.AddressBit, () => Client.SendGoalCompletion());
 
             Client.MonitorLocations(myLocations);
 
             Context.ConnectButtonEnabled = true;
         }
 
-        private void _deathlinkService_OnDeathLinkReceived(DeathLink deathLink)
-        {
+        private void _deathlinkService_OnDeathLinkReceived(DeathLink deathLink) {
             //Todo kill player
         }
-        private static void Client_ItemReceived(object? sender, ItemReceivedEventArgs e)
-        {
+
+        private static void Client_ItemReceived(object? sender, ItemReceivedEventArgs e) {
             LogItem(e.Item);
             var itemId = e.Item.Id;
             // Todo Give the player the item
         }
-        private void Client_MessageReceived(object? sender, Archipelago.Core.Models.MessageReceivedEventArgs e)
-        {
+
+        private void Client_MessageReceived(object? sender, Archipelago.Core.Models.MessageReceivedEventArgs e) {
             if (e.Message.Parts.Any(x => x.Text == "[Hint]: "))
             {
                 LogHint(e.Message);
             }
             Log.Logger.Information(JsonConvert.SerializeObject(e.Message));
         }
-        private static void LogItem(Item item)
-        {
+
+        private static void LogItem(Item item) {
             var messageToLog = new LogListItem(new List<TextSpan>()
             {
                 new TextSpan(){Text = $"[{item.Id.ToString()}] -", TextColor = Color.FromRgb(255, 255, 255)},
@@ -119,8 +112,8 @@ namespace MyGameAP
                 });
             }
         }
-        private static void LogHint(LogMessage message)
-        {
+
+        private static void LogHint(LogMessage message) {
             var newMessage = message.Parts.Select(x => x.Text);
 
             if (Context.HintList.Any(x => x.TextSpans.Select(y => y.Text) == newMessage))
@@ -140,22 +133,22 @@ namespace MyGameAP
                 });
             }
         }
-        private static void OnConnected(object sender, EventArgs args)
-        {
+
+        private static void OnConnected(object sender, EventArgs args) {
             Log.Logger.Information("Connected to Archipelago");
             Log.Logger.Information($"Playing {Client.CurrentSession.ConnectionInfo.Game} as {Client.CurrentSession.Players.GetPlayerName(Client.CurrentSession.ConnectionInfo.Slot)}");
         }
 
-        private static void OnDisconnected(object sender, EventArgs args)
-        {
+
+        private static void OnDisconnected(object sender, EventArgs args) {
             Log.Logger.Information("Disconnected from Archipelago");
         }
-        protected override Window CreateWindow(IActivationState activationState)
-        {
+
+        protected override Window CreateWindow(IActivationState activationState) {
             var window = base.CreateWindow(activationState);
             if (DeviceInfo.Current.Platform == DevicePlatform.WinUI)
             {
-                window.Title = "MyGameAP - My Game Archipelago Randomizer";
+                window.Title = "Guacamelee STCE Archipelago Randomizer";
 
             }
             window.Width = 600;
--- MyGameAP/Helpers.cs
+++ MyGameAP/Helpers.cs
@@ -6,22 +6,18 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyGameAP
-{
-    public class Helpers
-    {
-        public static List<Archipelago.Core.Models.Location> GetLocations()
-        {
-            var json = OpenEmbeddedResource("MyGameAP.Resources.Locations.json");
+namespace MyGameAP {
+    public class Helpers {
+        public static List<Archipelago.Core.Models.Location> GetLocations() {
+            var json = OpenEmbeddedResource("GuacameleeAP.Resources.Locations.json");
             var list = JsonConvert.DeserializeObject<List<Archipelago.Core.Models.Location>>(json);
             return list;
         }
-        public static string OpenEmbeddedResource(string resourceName)
-        {
+
+        public static string OpenEmbeddedResource(string resourceName) {
             var assembly = Assembly.GetExecutingAssembly();
             using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            using (StreamReader reader = new StreamReader(stream))
-            {
+            using (StreamReader reader = new StreamReader(stream)) {
                 string file = reader.ReadToEnd();
                 return file;
             }
